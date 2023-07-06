39be2439c08169e8881d041c924aa523fdf3c1a5
Cory Francis Myers <cfm@panix.com>
2023-07-06 16:05:38 -0700

    feat(Library): new()

--- src/exercises/day-1/book-library.rs
+++ src/exercises/day-1/book-library.rs
@@ -25,7 +25,9 @@ impl Book {
 // - `self` for unique access by value.
 impl Library {
     fn new() -> Library {
-        todo!("Initialize and return a `Library` value")
+        Library {
+            books: Vec::<Book>::new(),
+        }
     }
 
     //fn len(self) -> usize {
@@ -56,23 +58,28 @@ impl Library {
 fn main() {
     let library = Library::new();
 
-    //println!("The library is empty: library.is_empty() -> {}", library.is_empty());
-    //
-    //library.add_book(Book::new("Lord of the Rings", 1954));
-    //library.add_book(Book::new("Alice's Adventures in Wonderland", 1865));
-    //
-    //println!("The library is no longer empty: library.is_empty() -> {}", library.is_empty());
-    //
-    //
-    //library.print_books();
-    //
-    //match library.oldest_book() {
-    //    Some(book) => println!("The oldest book is {}", book.title),
-    //    None => println!("The library is empty!"),
-    //}
-    //
-    //println!("The library has {} books", library.len());
-    //library.print_books();
+    println!(
+        "The library is empty: library.is_empty() -> {}",
+        library.is_empty()
+    );
+
+    library.add_book(Book::new("Lord of the Rings", 1954));
+    library.add_book(Book::new("Alice's Adventures in Wonderland", 1865));
+
+    println!(
+        "The library is no longer empty: library.is_empty() -> {}",
+        library.is_empty()
+    );
+
+    library.print_books();
+
+    match library.oldest_book() {
+        Some(book) => println!("The oldest book is {}", book.title),
+        None => println!("The library is empty!"),
+    }
+
+    println!("The library has {} books", library.len());
+    library.print_books();
 }
 
 #[test]
